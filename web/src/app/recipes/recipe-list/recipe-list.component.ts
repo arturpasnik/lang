54b32bf71a7a8be3b31590ee61abe8124b518fa8
Artur Pasnikowski <arturpasnik@gmail.com>
2018-04-12 23:40:15 +0100

    Frontend: Recipe list

--- web/src/app/recipes/recipe-list/recipe-list.component.ts
+++ web/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Cheeseburger', 'Everyones favourite Cheeseburger.', 'https://d1nqx6es26drid.cloudfront.net/app/uploads/2015/04/04043431/product-cheeseburger.png'),
+    new Recipe('Quarter Pounder', 'Everyones favourite Quarter Pounder.', 'https://www.mcdonalds.co.za/sites/default/files/productThumbnail/mcd-food-beef-quarter-pounder-w-cheese.png')
+  ];
 
   constructor() { }
 
