59c86262c97be8b6f82c5b3a3858c94788141cc2
Artur Pasnikowski <arturpasnik@gmail.com>
2018-04-14 22:28:48 +0100

    Frontend: Ingredient service

--- web/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ web/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Ingredient} from '../../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list.service';
 
 @Component({
 	selector: 'app-shopping-edit',
@@ -11,9 +12,7 @@ export class ShoppingEditComponent implements OnInit
 	@ViewChild('nameInput') nameInputRef: ElementRef;
 	@ViewChild('amountInput') amountInputRef: ElementRef;
 
-	@Output() ingredientAdded = new EventEmitter<Ingredient>();
-
-	constructor()
+	constructor(private slService: ShoppingListService)
 	{
 	}
 
@@ -23,7 +22,7 @@ export class ShoppingEditComponent implements OnInit
 
 	onAddItem(){
 		const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
-		this.ingredientAdded.emit(newIngredient);
+		this.slService.addIngredient(newIngredient);
 	}
 
 }
---
+++ web/src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,22 @@
+import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from '@angular/core';
+
+export class ShoppingListService
+{
+	ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+
+	private ingredients: Ingredient[] = [
+		new Ingredient('bolka', 1),
+		new Ingredient('ser', 1),
+	];
+
+	getIngredients(){
+		return this.ingredients.slice();
+	}
+
+	addIngredient(ingredient: Ingredient){
+		this.ingredients.push(ingredient);
+		this.ingredientsChanged.emit(this.ingredients.slice());
+	}
+}
