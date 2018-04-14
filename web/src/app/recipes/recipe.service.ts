e61c3586ed9810a6eb62055814c0524e649c2cee
Artur Pasnikowski <arturpasnik@gmail.com>
2018-04-15 00:36:51 +0100

    Frontend: Added Ingredient to Recipe model

--- web/src/app/recipes/recipe.service.ts
+++ web/src/app/recipes/recipe.service.ts
@@ -1,6 +1,8 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
 
+import {Ingredient} from '../shared/ingredient.model';
+
 export class RecipeService
 {
 
@@ -10,11 +12,21 @@ export class RecipeService
 		new Recipe(
 			'Cheeseburger',
 			'Everyones favourite Cheeseburger.',
-			'https://d1nqx6es26drid.cloudfront.net/app/uploads/2015/04/04043431/product-cheeseburger.png'),
+			'https://d1nqx6es26drid.cloudfront.net/app/uploads/2015/04/04043431/product-cheeseburger.png',
+			[
+				new Ingredient('bulka',1),
+				new Ingredient('ser',2),
+			]
+		),
 		new Recipe(
 			'Quarter Pounder',
 			'Everyones favourite Quarter Pounder.',
-			'https://www.mcdonalds.co.za/sites/default/files/productThumbnail/mcd-food-beef-quarter-pounder-w-cheese.png')
+			'https://www.mcdonalds.co.za/sites/default/files/productThumbnail/mcd-food-beef-quarter-pounder-w-cheese.png',
+			[
+				new Ingredient('bulka',1),
+				new Ingredient('ogorek',1),
+			]
+		)
 	];
 
 	getRecipes(){
