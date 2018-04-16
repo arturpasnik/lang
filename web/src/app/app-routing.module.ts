9debb1c5beebce2228bb6d59059ccfff2b5f54d5
Artur Pasnikowski <artur.pasnikowski@sru.org.uk>
2018-04-16 09:01:52 +0100

    Frontend: Login form and route for it

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {RecipesStartComponent} from './recipes/recipes-start/recipes-start.component';
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
+import {LoginComponent} from './auth/login/login.component';
 
 const appRoutes: Routes = [
 	{path: '', redirectTo: '/recipes', pathMatch: 'full'},
+	{path: 'login', component: LoginComponent},
 	{path: 'recipes', component: RecipesComponent,
 		children: [
 			{path: '', component: RecipesStartComponent},
