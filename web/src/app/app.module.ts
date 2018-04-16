fd0c40c599dd022a1909deb3b9593fab487d6934
Artur Pasnikowski <artur.pasnikowski@sru.org.uk>
2018-04-16 11:57:58 +0100

    Frontend: Recipe edit form object created in TS using ReactiveFormsModule

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -37,6 +37,7 @@ import { RegisterComponent } from './auth/register/register.component';
   imports: [
     BrowserModule,
     FormsModule,
+	  ReactiveFormsModule,
     AppRoutingModule,
   ],
   providers: [ShoppingListService],
