ccb3435e78cf05609a8b93c5e437394738c2de5f
Artur Pasnikowski <artur.pasnikowski@sru.org.uk>
2018-04-17 09:42:00 +0100

    Backend: Migration for recipes and ingredients table

---
+++ database/migrations/2018_04_17_081906_recipe.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Recipe extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('recipes', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->text('desc');
+			$table->text('imagePath');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+}
