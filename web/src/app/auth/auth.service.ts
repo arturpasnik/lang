3f076f918ff5bfb823264914745b2fa7937cba88
Artur Pasnikowski <artur.pasnikowski@sru.org.uk>
2018-04-18 10:26:00 +0100

    Frontend: Auth service - not working yet

---
+++ web/src/app/auth/auth.service.ts
@@ -0,0 +1,8 @@
+import {User} from '../shared/model/user.model';
+
+export class AuthService
+{
+	register(user: User){
+		console.log(user);
+	}
+}
--- web/src/app/auth/register/register.component.ts
+++ web/src/app/auth/register/register.component.ts
@@ -1,11 +1,14 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, Injectable, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {AuthService} from '../auth.service';
+import {User} from '../../shared/model/user.model';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
 
 	@ViewChild('f') registerForm: NgForm;
@@ -13,13 +16,14 @@ export class RegisterComponent implements OnInit {
 	default = {
 	  gender: 'male'
   };
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   onSubmit(){
-
+		const user = new User(null, this.registerForm.value.name, this.registerForm.value.email, this.registerForm.value.password);
+		this.authService.register(user);
   }
 
 }
