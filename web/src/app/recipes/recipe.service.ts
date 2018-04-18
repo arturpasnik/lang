3781c9a14083c86c590cf6ff79fdad8148814c2f
Artur Pasnikowski <artur.pasnikowski@sru.org.uk>
2018-04-18 10:09:37 +0100

    Frontend: delete recipes from code - now recipes are get from server

--- web/src/app/recipes/recipe.service.ts
+++ web/src/app/recipes/recipe.service.ts
@@ -13,26 +13,7 @@ export class RecipeService
 {
 	recipesChanged = new Subject<Recipe[]>();
 
-	private recipes: Recipe[] = [
-		new Recipe(
-			'Cheeseburger',
-			'Everyones favourite Cheeseburger.',
-			'https://d1nqx6es26drid.cloudfront.net/app/uploads/2015/04/04043431/product-cheeseburger.png',
-			[
-				new Ingredient('bulka',1),
-				new Ingredient('ser',2),
-			]
-		),
-		new Recipe(
-			'Quarter Pounder',
-			'Everyones favourite Quarter Pounder.',
-			'https://www.mcdonalds.co.za/sites/default/files/productThumbnail/mcd-food-beef-quarter-pounder-w-cheese.png',
-			[
-				new Ingredient('bulka',1),
-				new Ingredient('ogorek',1),
-			]
-		)
-	];
+	private recipes: Recipe[];
 
 	constructor(private slService: ShoppingListService, private http: HttpClient){}
 
