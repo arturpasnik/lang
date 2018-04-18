7875f6d521b0a5e96728bdb6a94de8b92d75f0dd
Artur Pasnikowski <arturpasnik@gmail.com>
2018-04-19 00:31:19 +0100

    Backend: install barryvdh/laravel-cors and remove Cors middleware

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Http\Request;
 */
 
 
-Route::post('/user/register', 'APIRegisterController@register');
-Route::post('/user/login', 'APILoginController@login');
+Route::post('user/register', 'APIRegisterController@register');
+Route::post('user/login', 'APILoginController@login');
 
 Route::group(['middleware' => 'jwt.auth'], function(){
 	Route::get('/user', function() { return auth()->user();});
