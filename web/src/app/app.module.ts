09aaa6d8f8b8400aa7874709c2ddfb36796c388c
Artur Pasnikowski <artur.pasnikowski@sru.org.uk>
2018-04-24 10:42:59 +0100

    Frontend: Core Module

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,12 +3,9 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from './recipes/recipe.service';
-import { FilterExampleComponent } from './filter-example/filter-example.component';
-import { FilterPipe } from './shared/pipe/filter.pipe';
 import {AuthService} from './auth/auth.service';
 import {AuthGuardService} from './auth/auth.guard.service';
 import {UserService} from './shared/user.service';
@@ -16,21 +13,18 @@ import {RequestInterceptorService} from './auth/request-interceptor.service';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {AuthModule} from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import {CoreModule} from './core/core.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     //FilterExampleComponent,
-	  FilterPipe,
-    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
 	  HttpClientModule,
+	  CoreModule,
 	  ShoppingListModule,
 	  AuthModule,
 	  SharedModule
