cc603ed0611b90dc588ac298333e1f3ef4058727
Artur Pasnikowski <artur.pasnikowski@sru.org.uk>
2018-04-24 09:37:22 +0100

    Frontend: Shopping list Module

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import { LoginComponent } from './auth/login/login.component';
@@ -14,21 +11,19 @@ import { RegisterComponent } from './auth/register/register.component';
 import {RecipeService} from './recipes/recipe.service';
 import { FilterExampleComponent } from './filter-example/filter-example.component';
 import { FilterPipe } from './shared/pipe/filter.pipe';
-import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import {AuthService} from './auth/auth.service';
 import {AuthGuardService} from './auth/auth.guard.service';
 import {UserService} from './shared/user.service';
 import {RequestInterceptorService} from './auth/request-interceptor.service';
 import {RecipesModule} from './recipes/recipes.module';
 import {SharedModule} from './shared/shared.module';
+import {ShoppingListModule} from './shopping-list/shopping-list.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     LoginComponent,
     RegisterComponent,
 	  FilterExampleComponent,
@@ -36,10 +31,10 @@ import {SharedModule} from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     AppRoutingModule,
 	  HttpClientModule,
 	  RecipesModule,
+	  ShoppingListModule,
 	  SharedModule
   ],
   providers: [RecipeService, ShoppingListService, AuthService, AuthGuardService, UserService,
