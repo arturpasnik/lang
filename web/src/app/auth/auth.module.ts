66220686bcd9f9c158888495d1557238b4ed12fc
Artur Pasnikowski <artur.pasnikowski@sru.org.uk>
2018-04-24 09:48:50 +0100

    Frontend: Auth Module routes

--- web/src/app/auth/auth.module.ts
+++ web/src/app/auth/auth.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
+import {AuthRoutes} from './auth.routes';
 
 @NgModule({
 	declarations:[
@@ -11,7 +12,8 @@ import {RegisterComponent} from './register/register.component';
 	],
 	imports:[
 		CommonModule,
-		FormsModule
+		FormsModule,
+		AuthRoutes
 	]
 })
 export class AuthModule
