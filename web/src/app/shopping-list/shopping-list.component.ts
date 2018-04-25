82a606759609103a3deb408af004fd2795d3cd92
Artur Pasnikowski <artur.pasnikowski@sru.org.uk>
2018-04-25 10:40:48 +0100

    Frontend: Shopping list add ingredient using StoreModule

--- web/src/app/shopping-list/shopping-list.component.ts
+++ web/src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from './shopping-list.service';
-import {Subscription} from 'rxjs/Subscription';
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs/Observable';
 
@@ -10,27 +9,17 @@ import {Observable} from 'rxjs/Observable';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit, OnDestroy {
+export class ShoppingListComponent implements OnInit {
 
   ingredientsState: Observable<{ingredients: Ingredient[]}>
-  private subscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService, private store: Store<{shoppingList: {ingredients: Ingredient[]}}>) { }
 
   ngOnInit() {
     this.ingredientsState = this.store.select('shoppingList');
-    /*this.subscription =  this.shoppingListService.ingredientsChanged.subscribe(
-      (ingredients: Ingredient[]) => {
-        this.ingredients = ingredients;
-      }
-    );*/
   }
 
   onEditItem(index: number){
 		this.shoppingListService.startEditing.next(index);
-  }
-
-  ngOnDestroy(){
-	  this.subscription.unsubscribe();
   }
 }
